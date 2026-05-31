3e494df910b581ada9494c94d9d91e8e07a885db
kotuk0iq <hdyhhdh75762@gmail.com>
2026-05-31 23:49:45 +0600

    Update core.js

--- core.js
+++ core.js
@@ -6,7 +6,6 @@ const GradusStatic = {
     _config: { debug: false, dbFile: '/db.json' },
     _handlers: {},
 
-    // Регистрация кастомного обработчика
     registerHandler(name, fn) {
         this._handlers[name] = fn;
         if (this._config.debug) console.log('[CORE] Зарегистрирован обработчик:', name);
@@ -15,12 +14,24 @@ const GradusStatic = {
     async init(userConfig = {}) {
         Object.assign(this._config, userConfig);
 
-        // Инициализируем БД
         if (window.GradusDB) await GradusDB.init(this._config.dbFile, this._config.debug);
 
         // Вызываем initSite() пользователя, если определена
         if (typeof window.initSite === 'function') {
             await window.initSite();
+        }
+
+        // Если debug: false – включаем защиту от DevTools, иначе выводим сообщение
+        if (!this._config.debug) {
+            if (window.GradusWeb && GradusWeb.security) {
+                GradusWeb.security.enableDevToolsProtection(() => {
+                    alert('Обнаружены инструменты разработчика! Данные удалены.');
+                    GradusWeb.cache.clear();
+                    location.reload();
+                });
+            }
+        } else {
+            console.log('[CORE] Защита от DevTools отключена (debug: true)');
         }
 
         // Запускаем рендеринг после загрузки DOM
