bbfa5dd7d8b9dac9402a976842ec564894fd4b78
kotuk0iq <hdyhhdh75762@gmail.com>
2026-05-31 16:25:04 +0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navLinks = document.querySelectorAll('nav ul li a');
-    navLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            navLinks.forEach(l => l.classList.remove('active'));
-            link.classList.add('active');
-        });
-    });
-});
